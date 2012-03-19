d6707a4ff11f925289a1be5ba0d1abc2b73d4232
Bin4ry <andreas.makris@googlemail.com>
2012-03-19 22:44:38 +0100

    ProParts for ICS. For now only Latin-Keyboards, we still need special ones like arabic,thai,korean....
    Change-Id: Ic9a9b87e3e300317b8513f67986a334ad9be5cb8

---
+++ ProParts/src/com/cyanogenmod/ProParts/Constants.java
@@ -0,0 +1,9 @@
+package com.cyanogenmod.ProParts;
+
+/**
+ * User: Pedro Veloso pedronveloso@androidpt.com
+ */
+public interface Constants {
+    
+    public static String LOG_TAG = "pro_parts";
+}
