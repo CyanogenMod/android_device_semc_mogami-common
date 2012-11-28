4dc4d5035f82abcf1d022c8ce5894b4a46bc0c17
Alin Jerpelea <jerpelea@gmail.com>
2012-11-28 18:02:09 +0200

    mogami: compatibility with ics camera blob
    Change-Id: I3b264e29e6ba3c4881ab412406730df12108db87

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -22,13 +22,9 @@ BOARD_HOSTAPD_DRIVER             := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_wl12xx
 BOARD_WLAN_DEVICE                := wl12xx_mac80211
 BOARD_SOFTAP_DEVICE              := wl12xx_mac80211
-#WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wl12xx_sdio.ko"
-#WIFI_DRIVER_MODULE_NAME          := "wl12xx_sdio"
-#WIFI_FIRMWARE_LOADER             := ""
 USES_TI_MAC80211 := true
 
 COMMON_GLOBAL_CFLAGS += -DUSES_TI_MAC80211
-#BOARD_LEGACY_NL80211_STA_EVENTS := true
 
 BOARD_HAVE_BLUETOOTH_CUSTOM_HCIATTACH := true
 TARGET_CUSTOM_BLUEDROID := ../../../device/semc/mogami-common/bluedroid/bluetooth.c
@@ -36,5 +32,7 @@ TARGET_CUSTOM_BLUEDROID := ../../../device/semc/mogami-common/bluedroid/bluetoot
 BOARD_HAVE_QCOM_FM := true
 BOARD_HAVE_FM_RADIO_TI := true
 COMMON_GLOBAL_CFLAGS += -DQCOM_FM_ENABLED -DHAVE_SEMC_FM_RADIO
+
+BOARD_CPU_COLOR_CONVERT:= true
 
 -include device/semc/msm7x30-common/Android.mk
